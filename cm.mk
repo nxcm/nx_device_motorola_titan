cf473232036e1e299013ea4c892e6945b5fbb795
AragaoAnderson <andersonaragao@me.com>
2015-10-02 12:06:03 -0300

    Titan: Our devices use nx_
    Change-Id: I19a9f91060ec9abc1cd3aae3f57d44ebab6d6b96

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/motorola/titan/full_titan.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := nx_titan
 NX_MODEL_NAME := Moto G (2014)
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
